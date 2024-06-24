a9df5a8d89a1fe71acade1b338ebaacb502df419
Tan <tanhoang5558@gmail.com>
2024-06-24 16:00:50 +0700

    init

---
+++ src/homepage/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Banner from "./component/Banner";
+import Carousel from "./component/Carousel";
+
+function HomePage(){
+    return(
+        <div>
+            <Banner></Banner>
+            <Carousel></Carousel>
+        </div>
+    );  
+}
+export default HomePage;
---
+++ src/homepage/component/Banner.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Banner() {
+    return (
+        <div className="p-2 mb-2 bg-white">
+                <div className="container-fluid py-5 text-white d-flex 
+                justify-content-center align-items-center" >
+                    <div>
+                        <img src={require('./../../images/Banner1.png')} width={'1500px'} height={'500px'}></img>                        
+                    </div>
+                </div>
+        </div>
+    );
+}
+export default Banner;
